cbedd98295104b416ac8e893a4fa1391c026e808
dontknowwhatname <13752060859@163.com>
2024-11-20 16:33:14 +0800

    Web-Lab10

---
+++ WebApplication6/Controllers/RolesAdminController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using System.Net;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+
+namespace WebApplication6.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RolesAdminController : Controller
+    {
+        public RolesAdminController()
+        {
+
+        }
+        public RolesAdminController(ApplicationUserManager userManager,ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+        private ApplicationRoleManager _roleManager;
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+        // GET: RolesAdmin 
+        public ActionResult Index()
+        {
+            return View(RoleManager.Roles);
+        }
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            // Get the list of Users in this Role 
+            var users = new List<ApplicationUser>();
+            // Get the list of Users in this Role 
+            foreach (var user in UserManager.Users.ToList())
+            {
+                if (await UserManager.IsInRoleAsync(user.Id, role.Name))
+                {
+                    users.Add(user);
+                }
+            }
+            ViewBag.Users = users;
+            ViewBag.UserCount = users.Count();
+            return View(role);
+        }
+        // GET: RolesAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: RolesAdmin/Edit/5 
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            RoleViewModel roleModel = new RoleViewModel { Id = role.Id, Name = role.Name };
+            return View(roleModel);
+        }
+        // GET: RolesAdmin/Delete/5 
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+          
+        
+
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+    }
+
+
+}
